5d3638ddb16582ca684e253cdc704dfd96dba749
chokiproai <Dogaopc13@gmail.com>
2023-11-21 12:39:05 +0700

    ChatGPT V2

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -93,7 +93,7 @@ async function onConversation() {
     +uuid,
     {
       dateTime: new Date().toLocaleString(),
-      text: '',
+      text: '⚫',
       loading: true,
       inversion: false,
       error: false,
